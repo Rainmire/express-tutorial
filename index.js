be3c3ccc9effc8fdb9be6d3f9fc614014dd58144
Qiumin Yin <mqyin@g.ucla.edu>
2018-01-30 17:15:23 -0800

    add 404 route

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ var things = require('./things.js');
 //both index.js and things.js should be in same directory
 app.use('/things', things);
 
+app.get('/:id', function(req, res){
+   res.send('The id you specified is ' + req.params.id);
+});
+
+app.get('/:id/:test', function(req, res){
+   res.send('test ' + req.params.id + ' ' + req.params.test);
+});
+
+app.get('*', function(req, res){
+   res.send('Sorry, this is an invalid URL.');
+});
+
 app.listen(3000);
